1ea34821686c269191e01723768b5133e6ddd655
Eric Johnson <roerjo.personal@gmail.com>
2021-08-24 16:41:46 -0400

    use packer ami

--- main.tf
+++ main.tf
@@ -8,6 +8,40 @@ terraform {
 }
 
 provider "aws" {
-  region  = "us-eas-1"
-  profile = "default"
+  region  = "us-east-1"
+  profile = "roerjo-cloudcasts"
+}
+
+data "aws_ami" "app" {
+  most_recent = true
+
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+  filter {
+    name   = "tag:Component"
+    values = ["app"]
+  }
+  filter {
+    name   = "tag:Project"
+    values = ["cloudcast"]
+  }
+  filter {
+    name   = "tag:Environment"
+    values = ["staging"]
+  }
+
+  owners = ["self"]
+}
+
+resource "aws_instance" "cloudcasts_web" {
+  ami           = data.aws_ami.app.id
+  instance_type = "t3.micro"
+
+  root_block_device {
+    volume_size = 8 # GB
+    volume_type = "gp3"
+  }
+
 }
